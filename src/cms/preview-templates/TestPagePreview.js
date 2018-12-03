ae0e0e8769a52d6258bd3806617ba4768124ca1d
Chris Bingham <chris.bingham@aerian.com>
2018-12-03 21:43:17 +0000

    added new tab images

--- src/cms/preview-templates/TestPagePreview.js
+++ src/cms/preview-templates/TestPagePreview.js
@@ -3,14 +3,6 @@ import PropTypes from 'prop-types'
 import { TestPageTemplate } from '../../templates/test-page'
 
 const TestPagePreview = ({ entry, getAsset }) => {
-  const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs'])
-  const blurbs = entryBlurbs ? entryBlurbs.toJS() : []
-
-  const entryTestimonials = entry.getIn(['data', 'testimonials'])
-  const testimonials = entryTestimonials ? entryTestimonials.toJS() : []
-
-  const entryPricingPlans = entry.getIn(['data', 'pricing', 'plans'])
-  const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
 
   return (
     <TestPageTemplate
